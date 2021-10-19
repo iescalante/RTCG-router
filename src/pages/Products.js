c4176e86985d3214c2a9429c888851141b8775b9
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-10-18 21:50:29 -0400

    first commit

---
+++ src/pages/Products.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const Products = () => {
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">A Flower</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">A Car</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">A House</Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
